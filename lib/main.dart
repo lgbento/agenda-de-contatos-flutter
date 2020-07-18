1fff2a6ac446365ddcc1c0196fca6f1556e1ee9b
LuisDev <luisgabrielbento@hotmail.com>
2020-07-18 11:46:55 -0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:contatos/ui/home_page.dart';
+import 'package:flutter/material.dart';
+
+import 'ui/contact_page.dart';
+
+void main(){
+  runApp(MaterialApp(
+    home: HomePage(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
